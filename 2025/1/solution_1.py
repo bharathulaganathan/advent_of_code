0402492ca608afc858623e0260ceca7f267b83c4
Bharath <bharathulaganathan@gmail.com>
2025-12-02 16:20:42 +0000

    2025 day 1 one

---
+++ 2025/1/solution_1.py
@@ -0,0 +1,23 @@
+import os
+base_dir = os.path.dirname(os.path.abspath(__file__))
+file_name = "input.txt"
+file_path = os.path.join(base_dir, file_name)
+
+rotations = []
+with open(file_path, "r") as input:
+    for row in input:
+        rotations.append(row.strip())
+
+dial = 50
+stop = 0
+
+for rotation in rotations:
+    change = int(rotation[1:])
+    if rotation[0] == "R":
+        dial = (dial + change) % 100
+    else:
+        dial = abs((dial - change) % 100)
+    if dial == 0:
+        stop += 1
+
+print(stop)
