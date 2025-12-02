39d33790a1e73f8eb0816e977f368486ac960bd0
Bharath <bharathulaganathan@gmail.com>
2025-12-02 16:10:10 +0000

    removed print in tempate solution_.py

--- solution_.py
+++ solution_.py
@@ -2,7 +2,3 @@ import os
 base_dir = os.path.dirname(os.path.abspath(__file__))
 file_name = "input.txt"
 file_path = os.path.join(base_dir, file_name)
-
-print("Advent of Code {} Day {}")
-print(f"Part One: {}")
-print(f"Part Two: {}")
