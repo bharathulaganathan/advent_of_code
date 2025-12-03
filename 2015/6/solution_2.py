5d219c98a57256ae1f4abcc9851ce17f4370c141
Bharath <bharathulaganathan@gmail.com>
2025-12-03 08:30:08 +0000

    2015 day 6 both

---
+++ 2015/6/solution_2.py
@@ -0,0 +1,42 @@
+import os
+base_dir = os.path.dirname(os.path.abspath(__file__))
+file_name = "input.txt"
+file_path = os.path.join(base_dir, file_name)
+
+instructions = []
+
+with open(file_path, "r") as input:
+    for row in input:
+        if row[:4] == "turn":
+            row = row[5:]
+        row = row.strip().split()
+        row = {
+            "mode": row[0],
+            "start": list(map(int, row[1].split(","))),
+            "end": list(map(int, row[3].split(",")))
+            }
+        instructions.append(row)
+
+max_lights = 1000
+lights = [[0]*max_lights for _ in range(max_lights)]
+
+for switch in instructions:
+    for x in range(switch["start"][0], switch["end"][0]+1):
+        for y in range(switch["start"][1], switch["end"][1]+1):
+            if switch["mode"] == "toggle":
+                lights[x][y] += 2
+            elif switch["mode"] == "on":
+                lights[x][y] += 1
+            elif switch["mode"] == "off":
+                lights[x][y] -= 1
+                if lights[x][y] < 0:
+                    lights[x][y] = 0
+            else:
+                print("ERROR_1!")
+
+lit = 0
+for x in range(max_lights):
+    for y in range(max_lights):
+            lit += lights[x][y]
+
+print(lit)
