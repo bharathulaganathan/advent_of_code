84f7c7adb7dd91ef5c678980abdbb485f0e7feef
Bharath <bharathulaganathan@gmail.com>
2025-12-04 09:01:58 +0000

    2025 day3 part1 improved

--- 2025/3/solution_1.py
+++ 2025/3/solution_1.py
@@ -12,15 +12,13 @@ joltage = 0
 
 for bank in banks:
     highest_10s = "0"
-    highest_index = "0"
+    highest_1s = "0"
     for i in range(len(bank)-1):
         if bank[i] > highest_10s:
             highest_10s = bank[i]
-            highest_index = i
-    highest_1s = "0"
-    for j in range(int(highest_index+1), len(bank)):
-        if bank[j] > highest_1s:
-            highest_1s = bank[j]
+            highest_1s = bank[i+1]
+        elif bank[i+1] > highest_1s:
+            highest_1s = bank[i+1]
     joltage += int(highest_10s + highest_1s)
 
 print(joltage)
