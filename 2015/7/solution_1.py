1b449c1c3981e40ca46cc77be79206610b0219e5
Bharath <bharathulaganathan@gmail.com>
2025-12-05 12:36:36 +0000

    2015 day-7 part-1

---
+++ 2015/7/solution_1.py
@@ -0,0 +1,53 @@
+import os
+base_dir = os.path.dirname(os.path.abspath(__file__))
+file_name = "input.txt"
+file_path = os.path.join(base_dir, file_name)
+
+calcs = {}
+
+with open(file_path, "r") as input:
+    for row in input:
+        expression, value = row.split(" -> ")
+        calcs[value.strip()] = expression
+
+# to_eval = ["d", "e", "f", "g", "h", "i", "x", "y"]
+to_eval = ["a"]
+answers = []
+
+def eval(unknown):
+    try:
+        return int(unknown)
+    except:
+        pass
+    exps = calcs[unknown]
+    try:
+        x = int(exps)
+        calcs[unknown] = x
+        return x
+    except:
+        pass
+    if "AND" in exps:
+        l, r = exps.split(" AND ")
+        x = eval(l) & eval(r)
+    elif "OR" in exps:
+        l, r = exps.split(" OR ")
+        x = eval(l) | eval(r)
+    elif "LSHIFT" in exps:
+        l, r = exps.split(" LSHIFT ")
+        x = eval(l) << int(r)
+    elif "RSHIFT" in exps:
+        l, r = exps.split(" RSHIFT ")
+        x = eval(l) >> int(r)
+    elif "NOT" in exps:
+        l, r = exps.split("NOT ")
+        x = 2**16 + ~(eval(r))
+    else:
+        x = eval(exps)
+    calcs[unknown] = x
+    return x
+
+for unknown in to_eval:
+    answers.append(eval(unknown))
+
+for answer in answers:
+    print(answer)
