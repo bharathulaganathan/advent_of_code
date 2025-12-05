e6433cd09bd09f4803b0b2f02c915e0c909fdd92
Bharath <bharathulaganathan@gmail.com>
2025-12-05 13:29:25 +0000

    2012 Day 8 (Part 2)

--- 2015/8/solution_2.py
+++ 2015/8/solution_2.py
@@ -3,6 +3,27 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 file_name = "input.txt"
 file_path = os.path.join(base_dir, file_name)
 
+text = ""
 
+with open(file_path, "r") as input:
+    for row in input:
+        row = row.strip()
+        text += row
 
-print()
+total = 0
+i = 0
+while i < len(text):
+    if text[i] == "\\":
+        if text[i+1] == "x":
+            total += 1
+            i += 4
+        else:
+            total += 2
+            i += 2
+    elif text[i] == "\"":
+        total += 2
+        i += 1
+    else:
+        i += 1
+
+print(total)
